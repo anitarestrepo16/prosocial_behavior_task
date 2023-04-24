4a30abc40178fee758bc63ebbc5110c4506e7ebf
anitarestrepo16 <anitarestrepo16@gmail.com>
2023-04-24 16:20:19 -0500

    modifying john task

--- experiment.py
+++ experiment.py
@@ -5,22 +5,42 @@ from time import time
 
 from utils.ui import (
     fixation_cross,
+    present_text,
     wait_for_keypress,
-    wait_for_choice
+    present_choice,
+    work_rest_segment
 )
 
-# specify block design
-BLOCKS = OrderedDict()
-BLOCKS['baseline'] = (20, 0) # n_trials, milliseconds added delay
-BLOCKS['random1'] = (50, None) # None denotes random delay
-BLOCKS['adaption'] = (80, 200)
-BLOCKS['random2'] = (50, None)
+
 
 # initialize some things
 subj_num = input("Enter subject number: ")
 subj_num = int(subj_num)
 #log = TSVWriter(subj_num)
 np.random.seed(subj_num)
+# targets
+target_self = "You"
+target_other = "Next Participant"
+targets = [target_self, target_other]
+# trial types
+offer_reward = "Exert Effort to Win 10 Points for "
+offer_punishment = "Exert Effort to Avoid Losing 10 Points for "
+offers = [offer_reward, offer_punishment]
+
+# outcomes
+win_reward = "+10 Points for "
+win_punishment = "-0 Points for "
+lose_reward = "-0 Points for "
+lose_punishment = "-10 Points for "
+outcomes = [win_reward, win_punishment, lose_reward, lose_punishment]
+
+# specify block design -- n_trials, [target, trial_type]
+BLOCKS = OrderedDict()
+BLOCKS['self_reward'] = (25, ['self', 'reward']) 
+BLOCKS['self_punishment'] = (25, ['self', 'punishment']) 
+BLOCKS['other_reward'] = (25, ['other', 'reward'])
+BLOCKS['random2'] = (25, ['other', 'punishment'])
+########## still need to figure out how to randomize trial order
 
 win = visual.Window(
 	size = (800, 600),
@@ -52,23 +72,45 @@ for block_code in BLOCKS:
 	t2 = time()
 	print("\nBeginning %s block\n"%block_code)
 	print("It's been %d minutes since the experiment started."%((t2 - t1)/60))
-	des = BLOCKS[block_code]
-	n_trials = des[0]
+	trial_info = BLOCKS[block_code]
+	n_trials = trial_info[0]
 
 	for trial in range(n_trials):
 
 		# decide what to do this trial
-		delay = np.random.randint(0, 250) if des[1] is None else des[1]
-		sentence = sentences.pop(0)
+		### need to figure out how to randomize what shows up in this part and save data
+		
+		offer = offers[0] + targets[0] 
+
 
 		# keep experimenter in the loop via console
 		stdout.write("\rTrial {0:03d}".format(trial) + "/%d"%n_trials)
 		stdout.flush()
 
-		# now actually run the trial
-		wait_for_keypress(win, message = 'Press spacebar to continue.')
-		audio.set_delay(delay)
+		## now actually run the trial
+		# offer
+		present_text(win, offer)
+		# fixation
 		fixation_cross(win)
+		# choice
+		choice = present_choice(win)
+		# fixation
+		fixation_cross(win)
+		# work/rest
+		work_rest_segment(win, choice)
+		#### need to figure out how will determine whether work trial succeeded
+		success = True
+		# fixation
+		fixation_cross(win)
+		# feeback
+
+		# fixation (ITI)
+		fixation_cross(win)
+
+
+		wait_for_keypress(win, message = 'Press spacebar to continue.')
+		
+		
 		start_t = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 		display(win, sentence) # flips screen
 		marker.send(trial + 1) # mark with trial number, 1-indexed
--- utils/ui.py
+++ utils/ui.py
@@ -21,13 +21,6 @@ def present_text(win, text_block):
 	msg.draw()
 	win.flip()
 
-def wait_for_choice():
-	'''
-    Wait for keyboard press (left or right arrow keys)
-        and records keypress. 
-    '''
-	return event.waitKeys(['left', 'right'])
-
 def wait_for_keypress(win, message = ''):
 	'''
 	Wait until subject presses spacebar.
@@ -35,3 +28,74 @@ def wait_for_keypress(win, message = ''):
 	if message:
 		present_text(win, message)
 	event.waitKeys(keyList = ["space"]) # wait until subject responds
+
+def present_choice(win):
+	'''
+    Shows options (work vs. rest), waits for keyboard press
+	  (left or right arrow keys)
+	  and records keypress. 
+    '''
+	choices = ['Work', 'Rest']
+	left_choice_message = visual.TextStim(win, pos = [-3, +3],
+                                  text = 'Press the \nleft arrow key \nto ' + choices[0])
+	left_choice_message.draw()
+	right_choice_message = visual.TextStim(win, pos = [+3, +3],
+                                  text = ' Press the \nright arrow key \nto ' + choices[1])
+	right_choice_message.draw()
+	win.flip()
+	return event.waitKeys(['left', 'right'])
+
+def work_rest_segment(win, choice):
+	'''
+	If chose to work, presents  grip strength segment,
+	otherwise presents "Rest".
+	'''
+	# if choose to work
+	if 'left' in choice:
+		# Countdown to Grip
+		countdown_message = visual.TextStim(win, pos = [0, +3], text = '3')
+		countdown_message.draw()
+		win.flip()
+		core.wait(0.5)
+		countdown_message.text = '3, 2'
+		countdown_message.draw()
+		win.flip()
+		core.wait(0.5)
+		countdown_message.text = '3, 2, 1'
+		countdown_message.draw()
+		win.flip()
+		core.wait(0.5)
+
+		# Grip
+		grip_message = visual.TextStim(win, pos = [0, +3], text = "SQUEEZE")
+		grip_message.draw()
+		win.flip()
+		core.wait(1)
+
+	# if choose to rest
+	elif 'right' in choice:
+		# rest segment
+		rest_message = visual.TextStim(win, pos = [0, +3], text = 'You may rest.')
+		rest_message.draw()
+		win.flip()
+		core.wait(1)
+
+	# Anything else
+	else:
+		# catch all
+		error_message = visual.TextStim(win, pos = [0, +3], text = 'Please make a choice')
+		error_message.draw()
+		win.flip()
+		core.wait(1)
+
+def present_feedback(win, offer, choice, success):
+	# if chose rest
+	if 'right' in choice:
+		present_text(win, "No points earned or lost.")
+	
+	# if worked and succeeded
+	elif 'left' in choice & success:
+		if 
+		present_text(win, outcome)
+	
+	# if wo
